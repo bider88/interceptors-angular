852211142bb921f8876c2da58ae8e0fb0c2a4157
Irving Didier <bider88@gmail.com>
2019-05-25 13:41:03 -0500

    Implementing interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
+import { TokenInterceptorService } from './interceptors/token-interceptor.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptorService, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -16,10 +16,7 @@ export class UserService {
     let params = new HttpParams().append('page', '1');
     params = params.append('name', 'Irving Didier');
 
-    const headers = new HttpHeaders({
-      'token-user': 'qmaSADg5v4sfdZfg654aSDF3dasfASDsfg3a'
-    });
-    return this.httpClient.get(`https://reqresas.in/api/users`, { params, headers }).pipe(
+    return this.httpClient.get(`https://reqres.in/api/users`, { params }).pipe(
       map( res => res['data'] ),
       catchError( this.manageErrors )
     );
